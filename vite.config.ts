2f8f0d5cddc99354519aad423c64a3eb2d9edc7a
Adriel.ak.ak <adriel.ak.ak@gmail.com>
2025-09-26 18:28:44 +0200

    git

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true },
   build: { outDir: "dist" }
 });
